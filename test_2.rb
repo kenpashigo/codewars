f4f1647e6a25bcddf1ad1c043b5e3c3084df16bb
Paulo Marques <paulo.marques@indigoway.com>
2020-07-23 10:17:23 -0300

    Test 2

--- test_2.rb
+++ test_2.rb
@@ -1,11 +1,7 @@
 def find_it(seq)
-    list = []
-    for value in seq do
-        seq[value] = seq[value] == nil ? 0 : seq[value] + 1
-    end
-
-    puts list
-    return false
+    counts = Hash.new(0)
+    seq.each { |name| counts[name] += 1 }
+    counts.each { |key, value| if (value % 2) != 0 then return key end }
 end
 
 puts find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
